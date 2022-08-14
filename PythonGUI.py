54e95d1a427fc75fbdd0366651eb57994e6b50a3
Irina Mityugova <38368286+irinamityugova@users.noreply.github.com>
2022-08-13 20:17:13 -0700

    Clean Up

--- PythonGUI.py
+++ PythonGUI.py
@@ -1,49 +1,29 @@
-from fileinput import filename
 import tkinter as tk
 
+#GUI Frame
 root = tk.Tk()
-root.geometry('300x200')
-root.title("Irina's App")
+root.geometry('400x224')
+root.title("GUI Animated GIF with tk import only")
+frm = tk.Frame(root, background="dark blue", height="224", width="400").grid()
 
-frm = tk.Frame(root, background="dark blue", height="200", width="300").grid()
-
-frameCnt = 36
-frame = tk.PhotoImage(file="image.gif", format = 'gif -index 0')
-
-lbl = tk.Label(frm, background="gray", image=frame , height="200", width="300")
+#GIF Initiation
+img = tk.PhotoImage(file="image.gif", format = 'gif -index 0')
+lbl = tk.Label(frm, background="gray", image=img)
 lbl.grid(column=0, row=0)
-frameCnt=0
+imgCnt = 0
 
-def showGif(frameCnt):
+#GIF Image Iteration
+def showGif(imgCnt=imgCnt):
     
-    if frameCnt > 37:
+    if imgCnt > 37:
         return
-    print(">> frameCnt: "+str(frameCnt))
-    frame = tk.PhotoImage(file="image.gif", format = 'gif -index ' + str(frameCnt))
-    lbl.configure(image=frame)
-    lbl.image = frame
-    root.after(500, showGif(frameCnt+1))
+    nextimage = tk.PhotoImage(file="image.gif", format = 'gif -index ' + str(imgCnt))
+    lbl.configure(image=nextimage)
+    lbl.image = nextimage
+    lbl.after(50, showGif, imgCnt+1)
 
-"""def nextImg(i, frmt, max=37) :
-        print("next")
-        frmt = 'gif -index '+str(i)
-        print(frmt)
-        nextimage = tk.PhotoImage(file="image.gif", format = frmt)
-        lbl.configure(image=nextimage)
-        root.after(200, nextImg, i+1, label)
-        if i==max : return"""
+#Iteration Call
+showGif()
 
-"""def animate(i, max=37, label=lbl) :
-        img = tk.PhotoImage(file="image.gif", format = 'gif -index '+str(i))
-        #label.configure(image = tk.PhotoImage(file="image.gif", format = 'gif -index 20'))
-        
-        print(">> iteration: "+str(i))
-        print("label: " + str(label))
-        print("img: " + str(img))
-
-        if i==max : return print("done")
-        root.after(200, animate, i+1, label)
-"""
-root.after(500, showGif, frameCnt+1)
+#GUI Playloop
 root.mainloop()
-
