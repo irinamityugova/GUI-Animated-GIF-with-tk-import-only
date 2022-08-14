0f596fd5303d64e3ba1920b117abdb1959e2e6f8
Irina Mityugova <38368286+irinamityugova@users.noreply.github.com>
2022-08-13 19:51:48 -0700

    Recursive function

--- PythonGUI.py
+++ PythonGUI.py
@@ -12,13 +12,38 @@ frame = tk.PhotoImage(file="image.gif", format = 'gif -index 0')
 
 lbl = tk.Label(frm, background="gray", image=frame , height="200", width="300")
 lbl.grid(column=0, row=0)
+frameCnt=0
 
-def animate(i, max=37) :
-    for i in range(frameCnt) : 
-        frame = tk.PhotoImage(file="image.gif", format = 'gif -index '+str(i))
-        lbl.configure(image = frame)
-        print(i, str(frame))
+def showGif(frameCnt):
+    
+    if frameCnt > 37:
         return
+    print(">> frameCnt: "+str(frameCnt))
+    frame = tk.PhotoImage(file="image.gif", format = 'gif -index ' + str(frameCnt))
+    lbl.configure(image=frame)
+    lbl.image = frame
+    root.after(500, showGif(frameCnt+1))
 
-root.after(500, animate, 0)
+"""def nextImg(i, frmt, max=37) :
+        print("next")
+        frmt = 'gif -index '+str(i)
+        print(frmt)
+        nextimage = tk.PhotoImage(file="image.gif", format = frmt)
+        lbl.configure(image=nextimage)
+        root.after(200, nextImg, i+1, label)
+        if i==max : return"""
+
+"""def animate(i, max=37, label=lbl) :
+        img = tk.PhotoImage(file="image.gif", format = 'gif -index '+str(i))
+        #label.configure(image = tk.PhotoImage(file="image.gif", format = 'gif -index 20'))
+        
+        print(">> iteration: "+str(i))
+        print("label: " + str(label))
+        print("img: " + str(img))
+
+        if i==max : return print("done")
+        root.after(200, animate, i+1, label)
+"""
+root.after(500, showGif, frameCnt+1)
 root.mainloop()
+
